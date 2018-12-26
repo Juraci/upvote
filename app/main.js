3035c76dc92d0c87ec0f71197efc58fa78c2275c
Juraci Vieira <juraci.vieira@gmail.com>
2018-12-26 14:44:00 -0200

    computed properties

--- app/main.js
+++ app/main.js
@@ -2,5 +2,10 @@ new Vue({
   el: '#app',
   data: {
     submissions: Seed.submissions
+  },
+  computed: {
+    sortedSubmissions () {
+      return this.submissions.sort((a, b) => { return b.votes - a.votes });
+    }
   }
 });
