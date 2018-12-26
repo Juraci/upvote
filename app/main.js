908fe86ea5e2a85c38c25ddf6e6e6b84320e669c
Juraci Vieira <juraci.vieira@gmail.com>
2018-12-26 14:50:02 -0200

    handling interactions

--- app/main.js
+++ app/main.js
@@ -4,8 +4,14 @@ new Vue({
     submissions: Seed.submissions
   },
   computed: {
-    sortedSubmissions () {
+    sortedSubmissions() {
       return this.submissions.sort((a, b) => { return b.votes - a.votes });
+    }
+  },
+  methods: {
+    upvote(submissionId) {
+      const submission = this.submissions.find(submission => submission.id === submissionId);
+      submission.votes++;
     }
   }
 });
