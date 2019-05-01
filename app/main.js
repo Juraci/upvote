cedfa661f598a69c2920bbc01243896f41f2f095
Juraci Vieira <juraci.vieira@gmail.com>
2019-05-01 16:19:09 -0300

    extracting section to component

--- app/main.js
+++ app/main.js
@@ -1,3 +1,35 @@
+const submissionComponent = {
+  template:
+  `
+  <div style="display: flex; width: 100%">
+       <figure class="media-left">
+         <img class="image is-64x64" :src="item.submissionImage">
+       </figure>
+       <div class="media-content">
+         <div class="content">
+           <p>
+             <strong>
+               <a href="#" class="has-text-info">{{ item.title }}</a>
+               <span class="tag is-small">#{{ item.id }}</span>
+             </strong>
+             <br>{{ item.description }}</br>
+             <small class="is-size-7">
+               Submitted by: <img :src="item.avatar" class="image is-24x24">
+             </small>
+           </p>
+         </div>
+       </div>
+       <div class="media-right">
+         <span class="icon is-small" @click="upvote(item.id)">
+           <i class="fa fa-chevron-up"></i>
+           <strong class="has-text-info">{{ item.votes }}</strong>
+         </span>
+       </div>
+  </div>
+  `,
+  props: ['item', 'upvote'],
+};
+
 new Vue({
   el: '#app',
   data: {
@@ -16,5 +48,8 @@ new Vue({
       const item = this.submissions.find(item => item.id === id);
       item.votes++;
     }
+  },
+  components: {
+    'submission-component': submissionComponent,
   }
 });
