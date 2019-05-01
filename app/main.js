45148e89c7ae3c7a8e65dcb5fe87c8e04a53c038
Juraci Vieira <juraci.vieira@gmail.com>
2019-05-01 11:11:18 -0300

    exercise reset

--- app/main.js
+++ app/main.js
@@ -1,52 +1,6 @@
-const submissionComponent = {
-  template:
-  `
-      <div style="display: flex; width: 100%">
-        <figure class="media-left">
-          <img class="image is-64x64" :src="submission.submissionImage">
-        </figure>
-        <div class="media-content">
-          <div class="content">
-            <p>
-              <strong>
-                <a :href="submission.url" class="has-text-info">{{ submission.title }}</a>
-                <span class="tag is-small">#{{ submission.id }}</span>
-              </strong>
-              <br>{{ submission.description }}</br>
-              <small class="is-size-7">
-                Submitted by: <img :src="submission.avatar" class="image is-24x24">
-              </small>
-            </p>
-          </div>
-        </div>
-        <div class="media-right">
-          <span class="icon is-small" @click="$emit('vote', submission.id)">
-            <i class="fa fa-chevron-up"></i>
-            <strong class="has-text-info">{{ submission.votes }}</strong>
-          </span>
-        </div>
-      </div>
-  `,
-  props: ['submission']
-};
-
 new Vue({
   el: '#app',
   data: {
     submissions: Seed.submissions
-  },
-  computed: {
-    sortedSubmissions() {
-      return this.submissions.sort((a, b) => { return b.votes - a.votes });
-    }
-  },
-  components: {
-    'submission-component': submissionComponent
-  },
-  methods: {
-    upvote(submissionId) {
-      const submission = this.submissions.find(submission => submission.id === submissionId);
-      submission.votes++;
-    }
   }
 });
